9838c73417b31af25f7ee295858963c2a9653dbe
wgfire <364049242@qq.com>
2023-11-26 21:46:00 +0800

    [i18翻译功能] update(openai):优化提交逻辑

--- src/toolkit/openai/config.ts
+++ src/toolkit/openai/config.ts
@@ -1,14 +1,20 @@
-export const config = {
-	locals: [
-		{
-			zhCN: {
-				name: "中文",
-				path: "xxx.json"
-			},
-			enUS: {
-				name: "英文",
-				path: "xxx.json"
-			}
+export interface ConfigProvider {
+	locals: {
+		[key: string]: {
+			name: string
+			path: string
 		}
-	]
+	}
 }
+module.exports = {
+	locals: {
+		CN: {
+			name: "中文",
+			path: "./locals/cn.json"
+		},
+		EN: {
+			name: "英文",
+			path: "./locals/en.json"
+		}
+	}
+} as ConfigProvider
--- src/toolkit/openai/utils.ts
+++ src/toolkit/openai/utils.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios"
+import path from "node:path"
+import { ConfigProvider } from "./config"
 export const defaultToken = "sk-4161uzGojlRVmfngE1C516413c1b43F995D6AaD4C921597e"
 export const defaultApi = "https://api.gptapi.us"
+export const defaultPath = path.resolve(__dirname, "config")
+
 interface ChatCompletion {
 	id: string
 	object: string
@@ -29,46 +33,65 @@ const options: AxiosRequestConfig = {
 		Authorization: `Bearer ${defaultToken}`
 	}
 }
-const defaultData = {
-	model: "gpt-3.5-turbo",
-	messages: [
-		{
-			role: "user",
-			content: "你现在是一名专业的前端工程师，我将询问你有关前端的问题，涉及react,编码规范、组件设计相关前端知识点"
-		},
-		{
-			role: "user",
-			content: `
-			现在有一段这样的react代码，请将下面代码里的中文变成 t('ZHONG_WEN') t函数里面是对应的中文拼音，返回改变后的react代码给我
-			import react from 'react'
-			import { t } from 'react-i18next'
-			const button = ()=>{
-				const [state,setState]=useState(false)
-				return (
-					<div>
-					 <span>点击按钮</span>
-					 <span>点击查询</span>
-					 <section>展开面板</section>
-					 <section>用户实例</section>
-					</div>
-				)
-			}
-			`
-		},
-		{
-			role: "user",
-			content: `
-       好，下一步我需要你帮我将对应的拼音翻译成 
-			 两个语言，中文 ，英语 的json格式，拼音作为他的key
-			 如 {zh: { "TI_JIAO":"提交"}}
-			 将所有的语言生成一个index.json文件，
-			`
-		}
-	],
-	temperature: 0.7
+
+const getConfig = (configPath: string = defaultPath) => {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const data = require(configPath) as ConfigProvider
+	console.log(data, "data")
+	return data
 }
-export const requestOpenAi = async (data = defaultData) => {
-	options.data = data
+const getPrompt = () => {
+	const postData = {
+		model: "gpt-3.5-turbo",
+		messages: [
+			{
+				role: "user",
+				content: `你现在是一名专业的前端工程师，我将询问你有关前端的问题，涉及react,编码规范、组件设计相关前端知识点`
+			},
+			{
+				role: "user",
+				content: `
+				现在有一段这样的react代码，请将下面代码里的中文变成 t('ZHONG_WEN') t函数里面是对应的中文拼音，返回改变后的react代码给我
+				import react from 'react'
+				import { t } from 'react-i18next'
+				const button = ()=>{
+					const [state,setState]=useState(false)
+					return (
+						<div>
+						 <span>点击按钮</span>
+						 <span>点击查询</span>
+						 <section>展开面板</section>
+						 <section>用户实例</section>
+						</div>
+					)
+				}
+				`
+			},
+			{
+				role: "user",
+				content: `
+				 好，下一步我需要你帮我将对应的拼音翻译成 
+				 两个语言，中文 ，英语 的json格式，拼音作为他的key
+				 如 { "TI_JIAO":"提交"}
+				`
+			},
+			{
+				role: "user",
+				content: `
+				 最后，当你明白我的意思后，依据下面这个这个对象，
+				 ${JSON.stringify(getConfig().locals)}
+				 将对应的翻译内容放到每个对象里的content这个key中返回给我形成一个新的locals对象
+				`
+			}
+		],
+		temperature: 0.8
+	}
+	return postData
+}
+export const requestOpenAi = async () => {
+	options.data = getPrompt()
+	console.log(options.data, "发生内容")
+
 	const result = await axios<ChatCompletion>(options)
 	for (const choice of result.data.choices) {
 		console.log(choice.message.content, "返回结果")
